e8116bfdd53ccb6a75fe635ea32fd6c87d412b84
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2026-02-02 16:55:40 +0100

    chore: remove unused route files and documentation from the routes folder

--- backend-fs/src/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-
-const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-	fastify.get('/', async (request, reply) => ({ root: true }));
-};
-
-export default root;
