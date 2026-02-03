83ac45d918f8e7541857e2897d1f546f2805d35e
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2026-02-03 18:32:17 +0100

    refactor: update user repository and router to use NewUser type and integrate user service for improved user creation handling

--- backend-fs/src/modules/user/user.repository.ts
+++ backend-fs/src/modules/user/user.repository.ts
@@ -1,12 +1,10 @@
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { NewUser, User, usersTable } from '../../db/schema';
-import { CreateUserDto } from './user.schema';
-
 export interface UserRepository {
-	createUser: (dto: CreateUserDto) => Promise<User>;
+	createUser: (dto: NewUser) => Promise<User>;
 }
 
-export function createUserRepository(db: NodePgDatabase) {
+export function createUserRepository(db: NodePgDatabase): UserRepository {
 	return {
 		createUser: async (newUser: NewUser) => {
 			const result = await db.insert(usersTable).values(newUser).returning();
--- backend-fs/src/modules/user/user.router.ts
+++ backend-fs/src/modules/user/user.router.ts
@@ -1,18 +1,24 @@
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import { FastifyInstance } from 'fastify';
+import { createUserRepository } from './user.repository';
 import { createUserSchema } from './user.schema';
+import { createUserService } from './user.service';
 
 export default async function userRouter(fastify: FastifyInstance) {
+	const repo = createUserRepository(fastify.db);
+	const service = createUserService(repo);
+
 	fastify.withTypeProvider<TypeBoxTypeProvider>().post('/', {
 		schema: {
 			body: createUserSchema,
 			response: {
-				201: createUserSchema,
+				//201: createUserSchema,
 			},
 		},
 		handler: async (request, reply) => {
+			const user = await service.createUser(request.body);
 			reply.code(201);
-			return request.body;
+			return user;
 		},
 	});
 }
